7413b124091f4b0d67ba18e1de6c0c0d0caa7d0e
ingridmicaa <ingridmicaela03@gmail.com>
2025-10-29 12:52:32 -0500

    Espacios

---
+++ Ejemplo01/src/ejemplo01/Ejemplo061.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo061 {
+    public static void main(String[] args) {
+        // Uso de printf
+        // f: formato
+        
+        String nombreEstudiante = "Ingrid Micaela";
+        String apellidoEstudiante = "Pardo Gualan";
+        int edad = 17;
+        String direccion = "Los Geranios";
+        String universidad = "UTPL";
+        String ciclo= "Primer ciclo";
+        String asignatura = "Introducción a la programación";
+        String paralelo= "A";
+                
+        
+        
+        
+        // System.out.println(nombreEstudiante+" "+apellidoEstudiante+ " "+ 
+        // nacimiento);
+        // En printf; 
+        // usamos %s para reemplazar las cadenas, enteros, decimales
+        // usamos %d para reemplazar los valores enteros
+        // usamos %f para reemplazar los valore decimales
+        // System.out.printf("%s\n\n%s\n\n%s\n", nombreEstudiante, 
+        //        apellidoEstudiante, nacimiento);
+        System.out.printf("Nombre:\n\t%s\n Apellido:\n\t%s ", nombreEstudiante, apellidoEstudiante, edad);
+        
+        ;
+    }
+}

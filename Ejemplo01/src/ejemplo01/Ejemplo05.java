7289b8b2810d74b024e38d6d01f0f1a558d49a68
ingridmicaa <ingridmicaela03@gmail.com>
2025-10-29 12:14:10 -0500

    n y t

--- Ejemplo01/src/ejemplo01/Ejemplo05.java
+++ Ejemplo01/src/ejemplo01/Ejemplo05.java
@@ -27,6 +27,7 @@ public class Ejemplo05 {
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
         System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);   
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);  
+        // \n\t es salto de line y \t es 4 espacios un tabulador
     }
 }

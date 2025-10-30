1c524bbc4a12f2d1ec5db857e19529870b772580
ingridmicaa <ingridmicaela03@gmail.com>
2025-10-30 11:05:04 -0500

    Ejemplo 2

---
+++ Ejemplo02/src/paquete2/Ejemplo0311.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo0311 {
+    public static void main(String[] args) {
+        // Ingreso de datos por teclado
+        // uso de la clase Scanner
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        String apellido;
+        int nacimiento;
+        int ciclo;
+        
+        System.out.println("Ingrese el nombre del estudiante");
+        nombre = entrada.nextLine(); // nextLine: obtiene una cadena ingresada
+                                    // por teclado
+        
+        System.out.println("Ingrese el apellido del estudiante");
+        apellido = entrada.nextLine();
+        
+        
+        System.out.println("Ingrese el año de nacimiento del estudiante");
+        nacimiento = entrada.nextInt();
+        System.out.println("Ingrese su ciclo académico en número");
+        ciclo = entrada.nextInt();
+        
+        System.out.printf("Datos del estudiante\n Nombre: %s\n Apellido:"
+                + " %s\n Año de nacimiento: %d\n Ciclo académico:%d?+\n", 
+                nombre,
+                apellido,
+                nacimiento,
+                ciclo);
+        //hemos cambiado el system printf El porcentaje s a porcentaje d porque
+        //representa un numero entero
+    }
+}
